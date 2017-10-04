08b09caca94b57d043ea091ece1abbc04e33ed7a
PaulaCarrasco <paula-vanessa@misena.edu.co>
2017-10-03 23:51:51 -0500

    Primer Commit
    Gessy pullea esto papu

---
+++ src/app/raiders/raiders.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'raiders',
+	templateUrl: './raiders.component.html'
+	
+
+})
+
+export class raidersComponents{
+	public listado_raiders = 'listado de raiders';
+}
